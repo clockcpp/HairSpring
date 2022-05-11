2fd73a3645bda74a1b006b3fc0baa1d7e5730c54
ExcecuteIf <68136140+ExecuteIF@users.noreply.github.com>
2022-05-11 11:09:11 +0800

    Add files via upload

--- hairSpring.h
+++ hairSpring.h
@@ -667,6 +667,24 @@ namespace hs
         }
         return false;
     }
+
+    /// <summary>
+    /// don't forget there's a WINAPI called messageBeep()!
+    /// </summary>
+    /// <param name="type">see defitions like MSG_BEEP</param>
+    inline void playBeepMsg(int type)
+    {
+        MessageBeep(type);
+    }
+    /// <summary>
+    /// don't forget there's a WINAPI called beep()!
+    /// </summary>
+    /// <param name="freq"></param>
+    /// <param name="duration"></param>
+    inline void playBeepFreq(int freq, int duration)
+    {
+        Beep(freq, duration);
+    }
     /// <summary>
     /// read a bool value from a config file
     /// config file:
