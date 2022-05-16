55f04cd73a1ecd70a5842e79170ecaf70cdd095c
Excecutif <68136140+ExecuteIF@users.noreply.github.com>
2022-05-16 15:13:53 +0800

    Add files via upload

--- hairSpring.h
+++ hairSpring.h
@@ -852,7 +852,6 @@ namespace hs
         {
             ans.X = inf.dwMaximumWindowSize.X;
             ans.Y = inf.dwMaximumWindowSize.Y;
-
         }
         return ans;
     }
@@ -864,15 +863,49 @@ namespace hs
     {
         // thanks *anonymoususer__* at CSDN
         COORD ans = { -1,-1 };
-        HWND h = GetForegroundWindow();
+
         HANDLE hOutput = GetStdHandle(STD_OUTPUT_HANDLE);
         CONSOLE_FONT_INFO consoleCurrentFont;
         POINT p;
         GetCursorPos(&p);
-        ScreenToClient(h, &p);
+        ScreenToClient(cfg.consoleHwnd, &p);
         GetCurrentConsoleFont(hOutput, FALSE, &consoleCurrentFont);
         ans.X = p.x /= consoleCurrentFont.dwFontSize.X;
         ans.Y = p.y /= consoleCurrentFont.dwFontSize.Y;
+
+        short ans_x_cpy = ans.X;
+        switch (consoleCurrentFont.dwFontSize.X)
+        {
+        case 5:
+            ans_x_cpy /= 15;
+            ans.X -= ans_x_cpy;
+            break;
+        case 6:
+            break;
+        case 7:
+            ans_x_cpy /= 20;
+            ans.X -= ans_x_cpy;
+            break;
+        default:
+            handler.stopGame = 1;
+            Sleep(max(cfg.max_sleep_time_client, cfg.max_sleep_time_server) * 2);
+            _HairSpring::setConsoleColor(HS_CONSOLE_COLOR_WHITE, HS_CONSOLE_COLOR_BLUE);
+            cmd("cls");
+            cmd("title :(");
+            {
+                cerr << "Error: Mouse operation doesn't support current font on your console!\n\n";
+                cerr << "Right click on the title bar of the window, go properties, font, then change the size of current font or\n";
+                cerr << "select another font and make sure font width is 5, 6 or 7 to continue!\n\n";
+                cerr << "this will only effect current executable file, so don't worry\n";
+                cerr << "about your console.\n\n";
+                cerr << "Current font width: {" << consoleCurrentFont.dwFontSize.X << "} (this will not change after modified, please execute this application again to check.)\n";
+                cerr << "\n\n\n\nERROR-CODE: [HS_MOUSE_POSITION_FONT_ERROR]\n\n";
+                cerr << "\nIf you cannot fix this problem yourself, go https://github.com/clockcpp/HairSpring/discussions to look for the same problem. If there's nothing, create a new general duscussion for help;\nIf you are sure this is a issue, go https://github.com/clockcpp/HairSpring/issues for help.\n---PRESS ANY KEY TO EXIT---";
+            }
+            cmd("pause >nul");
+            break;
+        }
+
         COORD size = getConsoleSize();
         if (ans.X >= 0 && ans.Y >= 0 && ans.X <= size.X && ans.Y <= ans.Y)
         {
